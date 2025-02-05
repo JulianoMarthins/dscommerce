e06c69859234d24e1db514058c5cff97ea2c88dd
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-02-05 09:55:39 -0300

    Initial commit

---
+++ src/test/java/br/com/julianomarthins/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julianomarthins.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
