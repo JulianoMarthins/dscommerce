b47b195ec82cbc3c36f61e9d6de0b453d5a6d69d
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-02-23 11:44:03 -0300

    Product Update
    Criação do métodos de atualização dos produtos.
    
    Foi criado método auxiliar no classe productServiece.
    
    Os testes no banco de dados foram positivos.

--- src/main/java/br/com/julianomarthins/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/julianomarthins/dscommerce/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package br.com.julianomarthins.dscommerce.controllers;
 
 import br.com.julianomarthins.dscommerce.dto.ProductDTO;
+import br.com.julianomarthins.dscommerce.entities.Product;
 import br.com.julianomarthins.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -35,10 +36,14 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
         dto = service.insert(dto);
-
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
 
+    @PutMapping(value = "{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 
 
