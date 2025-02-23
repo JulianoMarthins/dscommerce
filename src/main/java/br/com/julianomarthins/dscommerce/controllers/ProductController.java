0117cd957eecbfbb89986dc64626556b100b8c08
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-02-23 11:23:16 -0300

    Customização de respostas
    Foi criada a customização de respostas do servidor com o 'ResponseEntity"
    
    Todos os testes realizados tiveram retorno positivo.

--- src/main/java/br/com/julianomarthins/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/julianomarthins/dscommerce/controllers/ProductController.java
@@ -5,7 +5,11 @@ import br.com.julianomarthins.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -17,18 +21,23 @@ public class ProductController {
 
     // Mapeia esta função como "GET", o valor no argumento é o caminho do endpoint.
     @GetMapping(value = "/{id}")
-   public ProductDTO findById(@PathVariable Long id){ // Annotation PathVariable torta a variável Long id como o valor do caminho do produto
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) { // Annotation PathVariable torta a variável Long id como o valor do caminho do produto
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO dto){
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
 
     }
 
