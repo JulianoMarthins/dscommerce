5a9ac07cc9f26b6d243a8c7d4f6414ace3eddc4a
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2026-03-21 16:08:27 -0300

    commit
    Atualização e teste de acesso ao usuários cliente e administrador funcionando com sucesso.

--- src/main/java/br/com/julianomarthins/dscommerce/DscommerceApplication.java
+++ src/main/java/br/com/julianomarthins/dscommerce/DscommerceApplication.java
@@ -1,13 +1,20 @@
 package br.com.julianomarthins.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication  {
+
+
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
+
 
 }
--- src/main/java/br/com/julianomarthins/dscommerce/entities/User.java
+++ src/main/java/br/com/julianomarthins/dscommerce/entities/User.java
@@ -147,7 +147,7 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return this.password;
     }
 
     // Testes
